2fadaa27adf6521a49304d92de820a3643dac3db
pandachan123 <129178966+pandachan123@users.noreply.github.com>
2024-11-11 13:00:46 +0530

    Update FractionalKnapsack.cpp

--- FractionalKnapsack.cpp
+++ FractionalKnapsack.cpp
@@ -1,39 +1,45 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
+
 struct Item {
-    int weight, profit;
+    int weight, profit, index;  
 };
+
 bool compare(Item a, Item b) {
     double r1 = (double)a.profit / a.weight;
     double r2 = (double)b.profit / b.weight;
     return r1 > r2;
 }
+
 double FractionalKnapsack(int W, Item arr[], int n) {
     sort(arr, arr + n, compare);
     int curWeight = 0;
-    double FinalValue = 0.0;
-
+    double maxProfit = 0.0;
+    bool included[n] = {false};
     for (int i = 0; i < n; i++) {
         if (curWeight + arr[i].weight <= W) {
             curWeight += arr[i].weight;
-            FinalValue += arr[i].profit;
-            cout << "Item " << i + 1;
-            cout << " Ratio included: 1 (fully included)\n";
-        } else {
+            maxProfit += arr[i].profit;
+            included[arr[i].index] = true;
+            cout << "Item " << arr[i].index + 1 << " Ratio included: 1 (fully included)\n";
+        }
+        else {
             int remain = W - curWeight;
-            if (remain > 0) {
-                curWeight += remain;
-                double fraction = (double)remain / arr[i].weight;
-                FinalValue += (double)arr[i].profit * fraction;
-                cout << "Item " << i + 1;
-                cout << " Ratio included: " << fraction << " \n";
-            } else {
-                cout << "Item " << i + 1 << " excluded (not enough space)\n";
-            }
+            double fraction = (double)remain / arr[i].weight;
+            maxProfit += arr[i].profit * fraction;
+            curWeight = W;  
+            included[arr[i].index] = true;
+            cout << "Item " << arr[i].index + 1 << " Ratio included: " << fraction << " (fractionally included)\n";
+            break;  
         }
     }
-    return FinalValue;
+    for (int i = 0; i < n; i++) {
+        if (!included[i]) {
+            cout << "Item " << i + 1 << " not included\n";
+        }
+    }
+    return maxProfit;
 }
 int main() {
     int W, n;
@@ -43,13 +49,14 @@ int main() {
     cin >> W;
     Item arr[n];
     for (int i = 0; i < n; i++) {
+        arr[i].index = i;  // Track original index
         cout << "Enter the weight of Item " << i + 1 << ": ";
         cin >> arr[i].weight;
     }
     for (int i = 0; i < n; i++) {
         cout << "Enter the profit of Item " << i + 1 << ": ";
         cin >> arr[i].profit;
-    } 
-    cout << "\n" << FractionalKnapsack(W, arr, n) << " is maximum profit\n";
+    }
+    cout << "\nMaximum profit: " << FractionalKnapsack(W, arr, n) << endl;
     return 0;
 }
